c7cf01090a14d1502a7a6f14ecd83be213b30797
PedroYutaroUEA <pymmn.snf23@uea.edu.br>
2024-06-27 14:28:38 -0400

    create: myreactapp

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+function App() {
+  return (
+    <div className="bg-zinc-800 h-screen">
+      <h1 className="text-white text-6xl font-extrabold text-center">
+        Hello, world!
+      </h1>
+      <h2 className="text-white text-2xl font-extrabold text-center">
+        Welcome to your new React app.
+      </h2>
+      <p className="text-white text-lg text-center">
+        Get started by editing <code>src/App.tsx</code>.
+      </p>
+      <p className="text-white text-lg text-center">
+        <a
+          className="text-blue-300 hover:text-blue-400"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default App;
